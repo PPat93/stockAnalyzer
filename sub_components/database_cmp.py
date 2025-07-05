372229ab54ec98a5180e24c754f28d6333f37e6a
PPat93 <45639693+PPat93@users.noreply.github.com>
2025-07-05 18:33:59 +0100

    v0.12.1-minor-typo

--- sub_components/database_cmp.py
+++ sub_components/database_cmp.py
@@ -1,7 +1,7 @@
 """
 Database component:
 Sends/Receives data from Integrator: <->Analyzer, ->Mailer;
-Stores observed stocks and utilities data (names, tickers, observed exhange, currency, etc.);
+Stores observed stocks and utilities data (names, tickers, observed exchange, currency, etc.);
 Stores price and date/time data from previous price retrievals for each observed stock;
 Stores price and date/time reference values for each observed stock;
 """
