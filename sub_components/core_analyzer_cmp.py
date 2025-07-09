a16d05baac632510a3eea488d47de7b01c4792fc
PPat93 <45639693+PPat93@users.noreply.github.com>
2025-07-09 22:23:19 +0100

    v0.13.0 - finish fcff calc for the last 5 years

--- sub_components/core_analyzer_cmp.py
+++ sub_components/core_analyzer_cmp.py
@@ -17,6 +17,7 @@ class CoreStock:
         self.income_statements_data = None
         self.ticker = ticker
         self.core_item_retriever = None
+        self.fcff_array = []
 
     def get_cash_flows(self):
         """Get Cash Flows data for the ticker if they were not retrieved before."""
@@ -34,20 +35,23 @@ class CoreStock:
         """Calculate Free Cash Flows for the ticker for last 5 years
         Cash Flow from Operating Activities + Interest Expense - Tax Shield on Interest Expense - Capital Expenditures"""
 
-        self.get_cash_flows()
-        self.get_income_statements()
+        for i in range(5):
+            self.get_cash_flows()
+            self.get_income_statements()
 
-        cash_flow_from_operating_activities = int(self.cash_flows_data['annualReports'][0]['operatingCashflow'])
-        capex = int(self.cash_flows_data['annualReports'][0]['capitalExpenditures'])
-        interest_expense = int(self.income_statements_data['annualReports'][0]['interestExpense'])
+            cash_flow_from_operating_activities = int(self.cash_flows_data['annualReports'][i]['operatingCashflow'])
+            capex = int(self.cash_flows_data['annualReports'][i]['capitalExpenditures'])
+            interest_expense = int(self.income_statements_data['annualReports'][i]['interestExpense'])
 
-        # Calculate Tax Shield on Interest Expense
-        income_tax_expense = int(self.income_statements_data['annualReports'][0]['incomeTaxExpense'])
-        income_before_tax = int(self.income_statements_data['annualReports'][0]['incomeBeforeTax'])
-        tax_rate = income_tax_expense / income_before_tax
-        tax_shield_on_interest_expense = abs(tax_rate) * interest_expense
+            # Calculate Tax Shield on Interest Expense
+            income_tax_expense = int(self.income_statements_data['annualReports'][i]['incomeTaxExpense'])
+            income_before_tax = int(self.income_statements_data['annualReports'][i]['incomeBeforeTax'])
+            tax_rate = income_tax_expense / income_before_tax
+            tax_shield_on_interest_expense = abs(tax_rate) * interest_expense
+            i += 1
+            self.fcff_array.append(cash_flow_from_operating_activities + interest_expense + tax_shield_on_interest_expense - capex)
 
-        return cash_flow_from_operating_activities + interest_expense + tax_shield_on_interest_expense - capex
+        return self.fcff_array
 
     def core_analyze(self):
         """Create a retriever for specified ticket and invoke Free Cash Flow calculation."""
